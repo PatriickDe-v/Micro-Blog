141e9438af56a5a289d420e5990284b871c78870
Patrick Strassburger <patriick.dev@hotmail.com>
2023-02-18 19:30:09 -0300

    refactor: Refatoração de código

--- app/routes.py
+++ app/routes.py
@@ -26,5 +26,5 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash(f'Login request for user {form.username.data}, remember_me={form.remember_me.data}')
-        return redirect( url_for('/index'))
+        return redirect( url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
