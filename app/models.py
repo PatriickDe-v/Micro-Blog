edb70580790b2b645ded7db3dc2eb456ebab5a18
Patrick Strassburger <patriick.dev@hotmail.com>
2023-02-21 09:52:32 -0300

    refactor: Reescritos alguns trechos de código

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from app import db
 
 
-class User(db.Model): 
-    id = db.column(db.Integer, primary_key = True)
-    username = db.column(db.String(64), index = True, unique = True)
-    email = db.Column(db.String(120), index = True, unique = True)
-    password_hash = db.Column(db.Strint(128))
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
 
-    def _repr__(self):
-        return f'<User {self.username}>'
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
