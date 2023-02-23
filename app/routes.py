e85411954749ac00502f73648294842f6f0033a2
Patrick Strassburger <patriick.dev@hotmail.com>
2023-02-22 21:20:13 -0300

    feat: Sistema de logout do usuário

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import flash, redirect, render_template, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 
 from app import app
 from app.forms import LoginForm
@@ -36,3 +36,8 @@ def login():
         login_user(user, rememer=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
