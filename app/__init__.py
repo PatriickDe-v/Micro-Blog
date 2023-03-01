0377a102f4eb73e1eab8e290239047aaff13a706
Patrick Strassburger <patriick.dev@hotmail.com>
2023-02-28 21:32:11 -0300

    feat: Log de erros por e-mail

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -14,3 +17,21 @@ login.login_view = 'login' #uam função ou (endpoit) para exibição de login
 from app import errors, models, routes
 
 #set FLASK_APP=microblog.py
+
+if not app.debug: 
+    if app.config['MAIL_SERVER']: 
+        auth = None 
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']: 
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        secure = None 
+        if app.config['MAIL_USE_TLS']: 
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.loger.addHandler(mail_handler)
+
+    
